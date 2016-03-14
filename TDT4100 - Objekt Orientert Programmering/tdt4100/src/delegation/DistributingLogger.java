eca005a9942d8d3a15b9bb57544e4d1bab45f382
niklasmh <nikasmh@hotmail.com>
2016-03-14 21:06:12 +0100

    Added part 3

--- TDT4100 - Objekt Orientert Programmering/tdt4100/src/delegation/DistributingLogger.java
+++ TDT4100 - Objekt Orientert Programmering/tdt4100/src/delegation/DistributingLogger.java
@@ -1,5 +1,28 @@
 package delegation;
 
-public class DistributingLogger {
+import java.util.HashMap;
+import java.util.Map;
 
+public class DistributingLogger implements ILogger {
+    private Map<String, ILogger> loggers = new HashMap<String, ILogger>();
+    private ILogger errorLogger, warningLogger, infoLogger;
+
+    public DistributingLogger (ILogger errorLogger, ILogger warningLogger, ILogger infoLogger) {
+        setLogger(ILogger.ERROR, errorLogger);
+        setLogger(ILogger.WARNING, warningLogger);
+        setLogger(ILogger.INFO, infoLogger);
+    }
+
+    public void setLogger(String severity, ILogger logger) {
+        this.loggers.put(severity, logger);
+    }
+
+    @Override
+    public void log(String severity, String message, Exception exception) {
+        ILogger logger = loggers.get(severity);
+
+        if (logger != null) {
+            logger.log(severity, message, exception);
+        }
+    }
 }
