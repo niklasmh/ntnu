23f1f493c193c39171cf193edf676ec47549c26b
niklasmh <nikasmh@hotmail.com>
2016-03-22 15:33:25 +0100

    Adding all the untracked files

--- TDT4100 - Objekt Orientert Programmering/tdt4100/src/sudokuv2/FileHandler.java
+++ TDT4100 - Objekt Orientert Programmering/tdt4100/src/sudokuv2/FileHandler.java
@@ -1,5 +1,39 @@
 package sudokuv2;
 
-public class FileHandler {
+//import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 
+public class FileHandler {
+    private String filePath, content;
+
+    public FileHandler(String filePath) {
+        this.filePath = filePath;
+        this.content = getFileContent();
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getFileContent(String filePath) {
+
+        try {
+            Scanner scanner = new Scanner(new FileReader(filePath));
+
+            content = scanner.next();
+            scanner.close();
+
+            return content;
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not find file with filepath: " + filePath);
+        }
+
+        return "";
+    }
+
+    public String getFileContent() {
+        return getFileContent(this.filePath);
+    }
 }
