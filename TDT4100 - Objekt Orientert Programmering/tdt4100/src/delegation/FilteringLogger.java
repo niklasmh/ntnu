cd8a6c26aed0e9ea23d0678ed93a933c05fd6f29
niklasmh <nikasmh@hotmail.com>
2016-04-04 16:04:31 +0200

    Changed FiltreringsLogger to pass the forgotten test

--- TDT4100 - Objekt Orientert Programmering/tdt4100/src/delegation/FilteringLogger.java
+++ TDT4100 - Objekt Orientert Programmering/tdt4100/src/delegation/FilteringLogger.java
@@ -2,18 +2,21 @@ package delegation;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class FilteringLogger implements ILogger {
     private List<String> severities;
-    private String logMessage, format = "%s: %s (%s)";
+    //private String logMessage, format = "%s: %s (%s)";
     private OutputStream stream;
     private ILogger logger;
 
     public FilteringLogger(ILogger logger, String... severities) {
-        super();
-        Collections.addAll(this.severities, severities);
+        //super();
+        //Collections.addAll(this.severities, severities);
+        this.severities = new ArrayList<>(Arrays.asList(severities));
         this.logger = logger;
     }
 
@@ -32,8 +35,9 @@ public class FilteringLogger implements ILogger {
 
     @Override
     public void log(String severity, String message, Exception exception) {
-        logMessage = String.format(format, severity, message, exception);
-
-        logger.log(severity, logMessage, exception);
+        //logMessage = String.format(format, severity, message, exception);
+        if (isLogging(severity)) {
+            logger.log(severity, message, exception);
+        }
     }
 }
