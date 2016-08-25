c3b6a330f4aa638f2af03f28054be50cb6297e9e
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-08-25 19:57:44 +0200

    Added first exercise in TDT4120

---
+++ TDT4120 - Algoritmer og datastrukturer/exercises/1/solution.py
@@ -0,0 +1,25 @@
+from sys import stdin
+
+class Kubbe:
+    vekt = None
+    neste = None
+    def __init__(self, vekt):
+        self.vekt = vekt
+        self.neste = None
+
+def spor(kubbe):
+    # SKRIV DIN KODE HER
+
+# Oppretter lenket liste
+forste = None
+siste = None
+for linje in stdin:
+    forrige_siste = siste
+    siste = Kubbe(int(linje))
+    if forste == None:
+        forste = siste
+    else:
+        forrige_siste.neste = siste
+
+# Kaller loesningsfunksjonen og skriver ut resultatet
+print(spor(forste))
---
+++ TDT4120 - Algoritmer og datastrukturer/exercises/1/sprengstoff.py
@@ -0,0 +1,29 @@
+from sys import stdin
+
+class Kubbe:
+    vekt = None
+    neste = None
+    def __init__(self, vekt):
+        self.vekt = vekt
+        self.neste = None
+
+def spor(kubbe):
+    vekt = kubbe.vekt
+    while kubbe.neste is not None:
+        kubbe = kubbe.neste
+        vekt = kubbe.vekt if kubbe.vekt > vekt else vekt
+    return vekt
+
+# Oppretter lenket liste
+forste = None
+siste = None
+for linje in stdin:
+    forrige_siste = siste
+    siste = Kubbe(int(linje))
+    if forste == None:
+        forste = siste
+    else:
+        forrige_siste.neste = siste
+
+# Kaller loesningsfunksjonen og skriver ut resultatet
+print(spor(forste))
