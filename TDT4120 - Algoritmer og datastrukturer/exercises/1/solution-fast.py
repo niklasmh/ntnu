2645e8c90b912f5ac9748b64c105ed8aef94c8e6
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-08-28 18:10:13 +0200

    Added recursive method (if it doesn't exceed), and optimized linear method and input

--- TDT4120 - Algoritmer og datastrukturer/exercises/1/solution-fast.py
+++ TDT4120 - Algoritmer og datastrukturer/exercises/1/solution-fast.py
@@ -2,36 +2,29 @@
 
 from sys import stdin
 
-def spor(kubbe):
+# Rekursivt forsok
+def spor_rec (k, vekt):
+    if k[0] > vekt:
+        vekt = k[0]
+        print(vekt)
+    return spor_rec(k[1], vekt) if k[1] is not None else vekt
+
+# Lineaert forsok
+def spor_lin(kubbe):
     vekt = kubbe[0]
-    while kubbe[1] is not None:
-        kubbe = kubbe[1]
-        if kubbe[0] > vekt:
-            vekt = kubbe[0]
-    return vekt
+    while True:
+        try:
+            kubbe = kubbe[1]
+            if kubbe[0] > vekt:
+                vekt = kubbe[0]
+        except:
+            return vekt
 
 # Oppretter lenket liste
-forste = None
 siste = None
-forrige_vekt = 0
-forrige_neste = None
-init = True
-for linje in stdin:
-    if init:
-        forrige_vekt = int(linje)
-        forrige_neste = None
-        init = False
-    else:
-        forrige_neste = siste
-        siste = (
-            forrige_vekt,
-            forrige_neste
-        )
-        forrige_vekt = int(linje)
-siste = (
-    forrige_vekt,
-    siste
-)
+for linje in map(int, stdin):
+    siste = (linje, siste)
 
 # Kaller loesningsfunksjonen og skriver ut resultatet
-print(spor(siste))
+#print(spor_rec(siste, siste[0]))
+print(spor_lin(siste))
