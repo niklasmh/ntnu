a41606bc0c0b8b120abd8b0a4e102dd469432ecb
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-09-01 22:43:11 +0200

    Added a solution to assignment 2

---
+++ TDT4120 - Algoritmer og datastrukturer/exercises/2/solution.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+from sys import stdin, stderr
+import traceback
+
+class Node:
+    def __init__(self):
+        self.barn = {}
+        self.posi = []
+
+def bygg(ordliste):
+    node = Node()
+    snode = node
+    for element in ordliste:
+        node = snode
+        for i, letter in enumerate(element[0]):
+            if letter not in node.barn:
+                node.barn[letter] = Node()
+            if i + 1 >= len(element[0]):
+                node.barn[letter].posi.append(element[1])
+            node = node.barn[letter]
+    return snode
+
+def posisjoner(ord, indeks, node):
+    if len(ord) > indeks + 1:
+        return posisjoner(ord, indeks + 1, node.barn[ord[indeks]])
+    else:
+        return node.barn[ord[indeks]].posi
+
+def main():
+    try:
+        ord = stdin.readline().split()
+        ordliste = []
+        pos = 0
+        for o in ord:
+            ordliste.append((o, pos))
+            pos += len(o) + 1
+        toppnode = bygg(ordliste)
+        for sokeord in stdin:
+            sokeord = sokeord.strip()
+            print("%s:" % sokeord, end='')
+            posi = posisjoner(sokeord, 0, toppnode)
+            posi.sort()
+            for p in posi:
+                print(" %s" % p, end='')
+            print()
+    except:
+        traceback.print_exc(file=stderr)
+
+if __name__ == "__main__":
+    main()
