14d406dba5fc2efd963c7c8f9bb8164f63d7250e
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-09-15 13:53:51 +0200

    Added exercise 3 initial tasks

---
+++ TDT4120 - Algoritmer og datastrukturer/exercises/3/kortstokker.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from sys import stdin
+from itertools import repeat
+
+
+def merge(decks):
+    # SKRIV DIN KODE HER
+
+
+def main():
+    # Read input.
+    decks = []
+    for line in stdin:
+        (index, csv) = line.strip().split(':')
+        deck = list(zip(map(int, csv.split(',')), repeat(index)))
+        decks.append(deck)
+    # Merge the decks and print result.
+    print(merge(decks))
+
+
+if __name__ == "__main__":
+    main()
