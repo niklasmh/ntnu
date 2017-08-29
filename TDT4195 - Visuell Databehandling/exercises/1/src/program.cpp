c6ef543d112f0c46521bc6e83627bc165800f5e4
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-08-29 13:12:56 +0200

    Task 2 in TDT4195

--- TDT4195 - Visuell Databehandling/exercises/1/src/program.cpp
+++ TDT4195 - Visuell Databehandling/exercises/1/src/program.cpp
@@ -18,21 +18,9 @@ void runProgram(GLFWwindow* window)
     glClearColor(0.3f, 0.8f, 1.0f, 1.0f);
     
     static const GLfloat g_vertex_buffer_data[] = {
-        -0.6f, -0.6f, 0.0f,
-        0.6f, -0.6f, 0.0f,
-        0.0f,  0.6f, 0.0f,
-        -0.9f, -0.8f, 0.0f,
-        -0.7f, -0.8f, 0.0f,
-        -0.8f, -0.6f, 0.0f,
-        0.8f, 0.5f, 0.0f,
-        0.7f, 0.5f, 0.0f,
-        0.6f, 0.2f, 0.0f,
-        -0.8f, 0.8f, 0.0f,
-        -0.9f, 0.8f, 0.0f,
-        -0.8f, 0.6f, 0.0f,
-        0.9f, -0.8f, 0.0f,
-        0.7f, -0.8f, 0.0f,
-        0.8f, -0.9f, 0.0f,
+         0.6f, -0.8f, -1.2f,
+         0.0f,  0.4f,  0.0f,
+        -0.8f, -0.2f,  1.2f,
     };
 
     GLuint vertexbuffer = vertexArrayObject(g_vertex_buffer_data, sizeof(g_vertex_buffer_data));
