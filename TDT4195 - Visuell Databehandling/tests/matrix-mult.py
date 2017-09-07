757d447fbed9adee81f7592830fade10c999d42e
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-09-07 19:09:01 +0200

    Beautified the matrix multipier

--- TDT4195 - Visuell Databehandling/tests/matrix-mult.py
+++ TDT4195 - Visuell Databehandling/tests/matrix-mult.py
@@ -19,15 +19,21 @@ a = args[2:2 + w * h]
 b = args[2 + w * h:]
 
 print("Matrix 1:")
-print(a)
+for i in range(len(a)): print(a[i], end=' ' if (i + 1) % w else '\n')
 print()
 print("Matrix 2:")
-print(b)
+for i in range(len(b)): print(a[i], end=' ' if (i + 1) % w else '\n')
 print()
 print("m1 * m2 =")
 m = min(w, h)
+m3 = []
+
 for i in range(m**2):
   s = sum([a[w * (i // h) + e] * b[i % h + e * h] for e in range(w)])
-  #s = [str(a[w * (i // h) + e])+"*"+str(b[i % h + e * h]) for e in range(w)]
+  # s = [str(a[w * (i // h) + e])+"*"+str(b[i % h + e * h]) for e in range(w)]
   print(s, end=' ' if (i + 1) % m else '\n')
+  m3.append(s)
 
+print()
+print(m3)
+print(" ".join(map(str, m3)))
