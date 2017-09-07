064ec044585ae9fd8b1276c8452b1283bfe9987f
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-09-07 19:33:32 +0200

    Made more prettier print of matrices

--- TDT4195 - Visuell Databehandling/tests/matrix-mult.py
+++ TDT4195 - Visuell Databehandling/tests/matrix-mult.py
@@ -17,22 +17,29 @@ if len(args) < 2 + w + h:
 
 a = args[2:2 + w * h]
 b = args[2 + w * h:]
+s = max(len(str(min(a + b))), len(str(max(a + b))))
+def fs (n): return ('' if n < 0 else ' ') + str(n) + ' '*(s + (0 if n < 0 else -1) - len(str(n)))
 
 print("Matrix 1:")
-for i in range(len(a)): print(a[i], end=' ' if (i + 1) % w else '\n')
+s = max(len(str(min(a))), len(str(max(a))))
+for i in range(len(a)): print(fs(a[i]), end=' ' if (i + 1) % w else '\n')
 print()
 print("Matrix 2:")
-for i in range(len(b)): print(a[i], end=' ' if (i + 1) % w else '\n')
+s = max(len(str(min(b))), len(str(max(b))))
+for i in range(len(b)): print(fs(b[i]), end=' ' if (i + 1) % w else '\n')
 print()
 print("m1 * m2 =")
 m = min(w, h)
 m3 = []
 
 for i in range(m**2):
-  s = sum([a[w * (i // h) + e] * b[i % h + e * h] for e in range(w)])
-  # s = [str(a[w * (i // h) + e])+"*"+str(b[i % h + e * h]) for e in range(w)]
-  print(s, end=' ' if (i + 1) % m else '\n')
-  m3.append(s)
+  p = sum([a[w * (i // h) + e] * b[i % h + e * h] for e in range(w)])
+  # p = [str(a[w * (i // h) + e])+"*"+str(b[i % h + e * h]) for e in range(w)]
+  m3.append(p)
+
+s = max(len(str(min(m3))), len(str(max(m3))))
+for i in range(m**2):
+  print(fs(m3[i]), end=' ' if (i + 1) % m else '\n')
 
 print()
 print(m3)
