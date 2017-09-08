ac8aed2fd0bdb10cda7ec78c64fc79b9e2522d63
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-09-08 09:14:50 +0200

    Printing out the math behind the multiplication

--- TDT4195 - Visuell Databehandling/tests/matrix-mult.py
+++ TDT4195 - Visuell Databehandling/tests/matrix-mult.py
@@ -26,7 +26,7 @@ for i in range(len(a)): print(fs(a[i]), end=' ' if (i + 1) % w else '\n')
 print()
 print("Matrix 2:")
 s = max(len(str(min(b))), len(str(max(b))))
-for i in range(len(b)): print(fs(b[i]), end=' ' if (i + 1) % w else '\n')
+for i in range(len(b)): print(fs(b[i]), end=' ' if (i + 1) % h else '\n')
 print()
 print("m1 * m2 =")
 m = min(w, h)
@@ -34,10 +34,13 @@ m3 = []
 
 for i in range(m**2):
   p = sum([a[w * (i // h) + e] * b[i % h + e * h] for e in range(w)])
-  # p = [str(a[w * (i // h) + e])+"*"+str(b[i % h + e * h]) for e in range(w)]
+
+  r = " + ".join([str(a[w * (i // h) + e])+"*"+str(b[i % h + e * h]) for e in range(w)])
+  print(r, end=' | ' if (i + 1) % m else '\n')
   m3.append(p)
 
 s = max(len(str(min(m3))), len(str(max(m3))))
+print("=")
 for i in range(m**2):
   print(fs(m3[i]), end=' ' if (i + 1) % m else '\n')
 
