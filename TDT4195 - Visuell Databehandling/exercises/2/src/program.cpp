789a6ebb014144c5881e78eb4c7db1555116ab33
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-09-24 18:13:31 +0200

    Added assignment 2 in TDT4195

--- TDT4195 - Visuell Databehandling/exercises/2/src/program.cpp
+++ TDT4195 - Visuell Databehandling/exercises/2/src/program.cpp
@@ -5,14 +5,21 @@
 #include "gloom/gloom.hpp"
 #include "gloom/shader.hpp"
 
-GLuint vertexArrayObject(float *vertices, uint vertexCount, uint *indices, uint indexCount, float *colors, uint colorCount);
+GLuint vertexArrayObject(float *vertices, unsigned int vertexCount, unsigned int *indices, unsigned int indexCount, float *colors, unsigned int colorCount);
+
+//Oppgave 4c
+float rotX = 0;
+float rotY = 0;
+float cordX = 0;
+float cordY = 0;
+float cordZ = -3.0f;
 
 void runProgram(GLFWwindow* window)
 {
     // Enable depth (Z) buffer (accept "closest" fragment)
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_GREATER);
-    glClearDepth(-1);
+    //glEnable(GL_DEPTH_TEST);
+    //glDepthFunc(GL_GREATER);
+    //glClearDepth(-1);
 
     // Configure miscellaneous OpenGL settings
     glEnable(GL_CULL_FACE);
@@ -25,15 +32,15 @@ void runProgram(GLFWwindow* window)
     glClearColor(0.3f, 0.8f, 1.0f, 1.0f);
 
     float g_vertex_buffer_data[] = {
-        -0.4f,  -0.25f, 0.6f,
-         0.1f,  -0.25f, 0.6f,
-        -0.15f,  0.3f,  0.6f,
-        -0.1f,  -0.25f, 0.7f,
-         0.4f,  -0.25f, 0.7f,
-         0.15f,  0.3f,  0.7f,
-        -0.25f, -0.58f, 0.8f,
-         0.25f, -0.58f, 0.8f,
-         0.0f,  -0.03f, 0.8f,
+       -0.40f, -0.25f, 0.6f,
+        0.10f, -0.25f, 0.6f,
+       -0.15f,  0.30f, 0.6f,
+       -0.10f, -0.25f, 0.7f,
+        0.40f, -0.25f, 0.7f,
+        0.15f,  0.30f, 0.7f,
+       -0.25f, -0.58f, 0.8f,
+        0.25f, -0.58f, 0.8f,
+        0.00f, -0.03f, 0.8f,
     };
 
     float g_color_buffer_data[] = {
@@ -50,14 +57,13 @@ void runProgram(GLFWwindow* window)
 
     unsigned int g_index_buffer_data[] = { 0,1,2,3,4,5,6,7,8, };
 
-    float value = 0.0f;
     glm::mat4x4 identityMatrix = glm::mat4(1.0f);
     glm::mat4x4 perspectiveMatrix = glm::perspective(glm::radians(45.0f), 16.0f / 9.0f, 1.0f, 100.0f);
 
     GLuint vao = vertexArrayObject(
-      g_vertex_buffer_data, sizeof(g_vertex_buffer_data),
-      g_index_buffer_data, sizeof(g_index_buffer_data),
-      g_color_buffer_data, sizeof(g_color_buffer_data)
+        g_vertex_buffer_data, sizeof(g_vertex_buffer_data),
+        g_index_buffer_data, sizeof(g_index_buffer_data),
+        g_color_buffer_data, sizeof(g_color_buffer_data)
     );
 
     Gloom::Shader shader;
@@ -72,23 +78,23 @@ void runProgram(GLFWwindow* window)
 
         // Assign a value to our uniform variable
 
-        value += 0.1f;
-        glm::mat4x4 perspectiveMatrix = glm::perspective(glm::radians(45.0f), 16.0f / 9.0f, -10.0f, 100.0f);
-        glm::mat4 View = glm::lookAt(
-            glm::vec3(4,3,3),
-            glm::vec3(0,0,0),
-            glm::vec3(0,1,0)
-        );
-        glm::mat4x4 translation = glm::translate(glm::mat4(), glm::vec3(0.0f, 0.0f, 0.0f));
-        glm::mat4x4 rotX = glm::rotate(glm::radians(value), glm::vec3(1.0f, 0.0f, 0.0f));
-        glm::mat4x4 rotY = glm::rotate(glm::radians(value), glm::vec3(0.0f, 1.0f, 0.0f));
+        // translation
+        glm::mat4x4 translation = glm::translate(glm::mat4(), glm::vec3(cordX, cordY, cordZ));
+        glm::mat4x4 transposedTrans = glm::transpose(translation);
 
-        glm::mat4x4 finalTransformationMatrix = perspectiveMatrix * translation * identityMatrix;
+        // rotation
+        glm::mat4x4 rotXMatrix = glm::rotate(glm::radians(rotX), glm::vec3(1.0f, 0.0f, 0.0f));
+        glm::mat4x4 rotYMartix = glm::rotate(glm::radians(rotY), glm::vec3(0.0f, 1.0f, 0.0f));
+
+        glm::mat4x4 transposedRotX = glm::transpose(rotXMatrix);
+        glm::mat4x4 transposedRotY = glm::transpose(rotYMartix);
+
+        glm::mat4x4 finalTransformationMatrix = transposedRotX * transposedRotY * transposedTrans * perspectiveMatrix * identityMatrix;
 
         glUniformMatrix4fv(4, 1, GL_FALSE, glm::value_ptr(finalTransformationMatrix));
 
         // Draw your scene here
-        glDrawElements(GL_TRIANGLES, sizeof(g_index_buffer_data) / sizeof(uint), GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, sizeof(g_index_buffer_data) / sizeof(unsigned int), GL_UNSIGNED_INT, 0);
 
         // Handle other events
         glfwPollEvents();
@@ -101,7 +107,7 @@ void runProgram(GLFWwindow* window)
     shader.destroy();
 }
 
-GLuint vertexArrayObject(float *vertices, uint vertexCount, uint *indices, uint indexCount, float *colors, uint colorCount)
+GLuint vertexArrayObject(float *vertices, unsigned int vertexCount, unsigned int *indices, unsigned int indexCount, float *colors, unsigned int colorCount)
 {
     GLuint VertexArrayID;
     glGenVertexArrays(1, &VertexArrayID);
@@ -137,5 +143,45 @@ void handleKeyboardInput(GLFWwindow* window)
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
     {
         glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+    else if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+    {
+        cordY += 0.01;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+    {
+        cordY -= 0.01;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+    {
+        cordX -= 0.01;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+    {
+        cordX += 0.01;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+    {
+        cordZ += 0.01;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
+    {
+        cordZ -= 0.01;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        rotX += 1.0;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
+        rotX -= 1.0;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
+        rotY -= 1.0;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
+        rotY += 1.0;
     }
 }
