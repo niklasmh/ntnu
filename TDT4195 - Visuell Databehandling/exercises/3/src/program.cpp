12deac310305b6b3df2f1c05c2f892aeb09c57be
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-10-06 14:52:12 +0200

    Add more comments to code in TDT4195

--- TDT4195 - Visuell Databehandling/exercises/3/src/program.cpp
+++ TDT4195 - Visuell Databehandling/exercises/3/src/program.cpp
@@ -32,6 +32,7 @@ void runProgram(GLFWwindow* window)
     // Set default colour after clearing the colour buffer
     glClearColor(0.3f, 0.8f, 1.0f, 1.0f);
 
+    // Adding the index buffer for a cube of triangles created in createBox
     unsigned int g_index_buffer_data[] = {
         0, 1, 2,
         2, 1, 3,
@@ -47,25 +48,29 @@ void runProgram(GLFWwindow* window)
         7, 2, 3,
      };
 
+    // Adding a color buffer making the cube contain all color gradients
     float g_color_buffer_data[] = {
-        1.0f, 1.0f, 1.0f, 1.0f,
-        1.0f, 1.0f, 0.0f, 1.0f,
-        0.0f, 1.0f, 1.0f, 1.0f,
-        0.0f, 1.0f, 0.0f, 1.0f,
-        1.0f, 0.0f, 1.0f, 1.0f,
-        1.0f, 0.0f, 0.0f, 1.0f,
-        0.0f, 0.0f, 1.0f, 1.0f,
-        0.0f, 0.0f, 0.0f, 1.0f,
+        1.0f, 1.0f, 1.0f, 1.0f, // White
+        1.0f, 1.0f, 0.0f, 1.0f, // Yellow
+        0.0f, 1.0f, 1.0f, 1.0f, // Cyan
+        0.0f, 1.0f, 0.0f, 1.0f, // Green
+        1.0f, 0.0f, 1.0f, 1.0f, // Pink
+        1.0f, 0.0f, 0.0f, 1.0f, // Red
+        0.0f, 0.0f, 1.0f, 1.0f, // Blue
+        0.0f, 0.0f, 0.0f, 1.0f, // Black
     };
 
+    // Creating an identity and perspective matrix for later
     glm::mat4x4 identityMatrix = glm::mat4(1.0f);
     glm::mat4x4 perspectiveMatrix = glm::perspective(glm::radians(45.0f), 16.0f / 9.0f, 1.0f, 100.0f);
 
-    int pointsCount = 12 * 8; // triangles * edges
-    int ibdCount = sizeof(g_index_buffer_data);
-    int cbdCount = sizeof(g_color_buffer_data);
+    // Setting some constant variables for creating the initial VAOs
+    const int pointsCount = 12 * 8; // triangles * edges
+    const int ibdCount = sizeof(g_index_buffer_data);
+    const int cbdCount = sizeof(g_color_buffer_data);
+    const float scale = 0.1f;
 
-    float scale = 0.1f;
+    // Creating the initial VAOs
     GLuint vaoHead = vertexArrayObject(createBox(-4.0f, 24.0f, -4.0f, 8.0f, 8.0f, 8.0f, scale), pointsCount, g_index_buffer_data, ibdCount, g_color_buffer_data, cbdCount);
     GLuint vaoBody = vertexArrayObject(createBox(-4.0f, 24.0f, -2.0f, 8.0f, -12.0f, 4.0f, scale), pointsCount, g_index_buffer_data, ibdCount, g_color_buffer_data, cbdCount);
     GLuint vaoArmLeft = vertexArrayObject(createBox(-8.0f, 24.0f, -2.0f, 4.0f, -12.0f, 4.0f, scale), pointsCount, g_index_buffer_data, ibdCount, g_color_buffer_data, cbdCount);
@@ -73,6 +78,7 @@ void runProgram(GLFWwindow* window)
     GLuint vaoLegLeft = vertexArrayObject(createBox(-4.0f, 12.0f, -2.0f, 4.0f, -12.0f, 4.0f, scale), pointsCount, g_index_buffer_data, ibdCount, g_color_buffer_data, cbdCount);
     GLuint vaoLegRight = vertexArrayObject(createBox(0.0f, 12.0f, -2.0f, 4.0f, -12.0f, 4.0f, scale), pointsCount, g_index_buffer_data, ibdCount, g_color_buffer_data, cbdCount);
 
+    // Adding the VAOs to a list
     GLuint vaoList[] = {
         vaoHead,
         vaoBody,
@@ -82,6 +88,7 @@ void runProgram(GLFWwindow* window)
         vaoLegRight,
     };
 
+    // Activating vertex and fragment shaders from text files
     Gloom::Shader shader;
     shader.makeBasicShader("../gloom/shaders/simple.vert", "../gloom/shaders/simple.frag");
     shader.activate();
@@ -94,24 +101,23 @@ void runProgram(GLFWwindow* window)
 
         // Assign a value to our uniform variable
 
-        // translation
+        // Translation
         glm::mat4x4 translation = glm::translate(glm::mat4(), glm::vec3(cordX, cordY, cordZ));
         glm::mat4x4 transposedTrans = glm::transpose(translation);
 
-        // rotation
+        // Rotation
         glm::mat4x4 rotXMatrix = glm::rotate(glm::radians(rotX), glm::vec3(1.0f, 0.0f, 0.0f));
         glm::mat4x4 rotYMartix = glm::rotate(glm::radians(rotY), glm::vec3(0.0f, 1.0f, 0.0f));
 
         glm::mat4x4 transposedRotX = glm::transpose(rotXMatrix);
         glm::mat4x4 transposedRotY = glm::transpose(rotYMartix);
 
-        int listSize = sizeof(vaoList);
         glm::mat4x4 finalTransformationMatrix = transposedRotX * transposedRotY * transposedTrans * perspectiveMatrix * identityMatrix;
         glUniformMatrix4fv(4, 1, GL_FALSE, glm::value_ptr(finalTransformationMatrix));
-
+        
+        // Drawing VAOs from the VAO array
+        int listSize = sizeof(vaoList);
         for (int i = 0; i < listSize; i++) {
-
-            // Draw your scene here
             glBindVertexArray(vaoList[i]);
             glDrawElements(GL_TRIANGLES, sizeof(g_index_buffer_data) / sizeof(unsigned int), GL_UNSIGNED_INT, (void*)0);
         }
