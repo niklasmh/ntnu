3399a8e92517e670f8866031c835d3f0033a5f6e
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-10-26 19:35:58 +0200

    Remove comments from perceptron

--- TDT4137 - Kognitive arkitekturer/exercises/4/a.py
+++ TDT4137 - Kognitive arkitekturer/exercises/4/a.py
@@ -63,28 +63,3 @@ perceptron([
   ([0], 1),
   ([1], 0)
 ])
-
-
-#from random import choice
-#from numpy import array, dot, random
-#unit_step = lambda x: 0 if x < 0 else 1
-#
-#training_data = [ (array([0,0]), 0), (array([0,1]), 0), (array([1,0]), 0), (array([1,1]), 1) ]
-#training_data = [ (array([0,0]), 0), (array([0,1]), 1), (array([1,0]), 1), (array([1,1]), 1) ]
-##training_data = [ (array([0]), 0), (array([1]), 1) ]
-#
-#w = random.rand(2)
-#errors = []
-#eta = 0.2
-#n = 100
-#
-#for i in range(n):
-#  x, expected = choice(training_data)
-#  result = dot(w, x)
-#  error = expected - unit_step(result)
-#  errors.append(error)
-#  w += eta * error * x
-#
-#for x, _ in training_data:
-#  result = dot(x, w)
-#  print("{}: {} -> {}".format(x, result, unit_step(result)))
