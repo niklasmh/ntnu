b2cd407e7ae40011857a40a9584a916f1335d491
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-10-31 17:32:37 +0100

    Add 2 subtasks for a3 in tdt4117

--- TDT4117 - Informasjonsgjenfinning/exercises/3/main.py
+++ TDT4117 - Informasjonsgjenfinning/exercises/3/main.py
@@ -38,10 +38,16 @@ dictionary = gensim.corpora.Dictionary(paragraphs)
 # 2.1 Filter away stopwords
 stopwords = "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your".split(",")
 token2id = dictionary.token2id
-stop_ids = list(map(lambda s: token2id[s], filter(lambda s: s in token2id, stopwords)))
+stop_ids = [token2id[s] for s in stopwords if s in token2id]
 dictionary.filter_tokens(stop_ids)
 
 # 2.2 Map into Bags-of-Words
 corpus = [dictionary.doc2bow(p) for p in paragraphs]
 
-print(corpus[0])
+
+# 3.1 Build TF-IDF model
+tfidf_model = gensim.models.TfidfModel(corpus)
+
+# 3.2 BOW to TF-IDF weights
+#tfidf_corpus = tfidf_model
+print(tfidf_model)
